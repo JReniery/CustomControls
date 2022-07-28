7626d0c56b1cbc575b181327e3658a5d3dc26aec
JReniery Aguilera <jreniery09@outlook.es>
2022-07-28 17:58:24 -0600

    Add project files.

---
+++ CustomControls/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomControls
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SurveyPage();
+            MainPage.BindingContext = new ViewModels.SurveyViewModel();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CustomControls/Controls/FormEntry.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomControls.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FormEntry : Grid
+    {
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(Title), typeof(string), typeof(FormEntry), default(string), Xamarin.Forms.BindingMode.OneWay);
+        public string Title
+        {
+            get
+            {
+                return (string)GetValue(TitleProperty);
+            }
+
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(FormEntry), default(string), BindingMode.TwoWay);
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+
+        public FormEntry()
+        {
+            InitializeComponent();
+
+            title.Text = Title;
+            entry.Text = Text;
+            entry.TextChanged += OnTextChanged;
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Text = e.NewTextValue;
+        }
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == TitleProperty.PropertyName)
+            {
+                title.Text = Title;
+            }
+            else if (propertyName == TextProperty.PropertyName)
+            {
+                entry.Text = Text;
+            }
+        }
+    }
+}
